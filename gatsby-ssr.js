9f69420ab81322cbf0edd7c7dd52cadcd8d3d52a
Fong Say Kiat <65012113+fsk1997@users.noreply.github.com>
2022-07-31 02:56:42 +0800

    Progress commit

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,5 +1,5 @@
-import "./src/styles/global.css";
 import "./src/styles/fonts.css";
+import "./src/styles/global.css";
 import "windy-radix-palette/base.css";
 import "tippy.js/dist/tippy.css"; //https://github.com/atomiks/tippyjs-react
 import React from "react";
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -25,7 +25,7 @@ const Layout = ({ children }) => {
 
   return (
     <div className={`${darkMode ? 'dark' : 'light'} newBody bg-slate-1 text-slate-12 min-h-screen`}>
-      <header className="container">
+      <header className="container relative z-[5]">
         <nav className="flex item-center justify-between py-4 lg:py-9">
           <LogoLink />
           <div className="flex items-center gap-4">
@@ -51,28 +51,34 @@ const Layout = ({ children }) => {
       </header>
 
       <main>
-        <section className="bg-hero bg-plum-4 -mt-28 pt-32 lg:pt-28 pb-10 lg:pb-20">
+        <section className="bg-hero relative -mt-28 pt-32 lg:pt-28 pb-10 lg:pb-20">
+          <div className="bg-ring-wrapper absolute z-0 top-0 left-0 w-full h-full overflow-hidden">
+            <div className="bg-ring"></div>
+            <div className="bg-ring"></div>
+            <div className="bg-ring"></div>
+          </div>
+
           <div className="container flex flex-col lg:flex-row items-center">
-            <div className="mix-blend-luminosity w-full lg:w-1/2 relative z-[1] mt-6">
-              <h1 className="text-slate-8 dark:brightness-150 text-[3.5rem] lg:text-[3rem] xl:text-[3.8rem] tracking-[-0.05rem] leading-[110%] font-medium lg:whitespace-nowrap drop-shadow-2xl shadow-plum-8">
+            <div className="mix-blend-luminosity w-full lg:w-1/2 relative z-[1] lg:-mt-14">
+              <h1 className="text-slate-8 dark:brightness-150 text-[3.5rem] lg:text-[3rem] xl:text-[3.8rem] tracking-[-0.05rem] leading-[110%] font-medium lg:whitespace-nowrap transition-mid">
                 <span className="inline-block">Open Source,</span>
                 <br/>
                 <span className="inline-block ml-[9.5rem]">Plug-and-Play</span>
                 <br/>
                 <span className="inline-block ml-12">React UI Components</span>
               </h1>
-              <div className="mix-blend-normal">
-                <p className="lg:text-xl my-6">
-                  Experimental React UI Components with Plain CSS
-                </p>
-                <div className="flex items-center justify-start gap-4">
-                  <button className="btn btn-plum">button 1</button>
-                  <button>button 2</button>
-                </div>
-              </div>
             </div>
             <div className="w-full lg:w-1/2 relative z-0">
               <StaticImage src="../images/hero-image.png"/>
+            </div>
+          </div>
+          <div className="container relative z-[1] -mt-32">
+            <p className="lg:text-xl my-6">
+              Experimental React UI Components with Plain CSS
+            </p>
+            <div className="flex items-center justify-start gap-4">
+              <button className="btn btn-plum">button 1</button>
+              <button className="btn btn-plum btn-outline">button 2</button>
             </div>
           </div>
         </section>
