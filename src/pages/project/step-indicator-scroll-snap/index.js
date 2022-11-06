7f1eef6f031c67b621413e852ba60fd599b0c0da
Fong Say Kiat <65012113+fsk1997@users.noreply.github.com>
2022-11-06 12:50:55 +0800

    Create new project

---
+++ src/pages/project/step-indicator-scroll-snap/index.js
@@ -0,0 +1,53 @@
+import React, { useEffect, useRef, useState } from "react";
+import * as styles from "./index.module.css";
+
+// Non essential imports
+import ProjectLayout from "../../../components/Layouts/ProjectLayout";
+import { useStaticQuery, graphql } from "gatsby";
+import useSiteMetadata from "../../../hooks/useSiteMetadata";
+import slugify from "react-slugify";
+import Seo from "../../../components/Seo";
+
+const Project = () => {
+  return (
+    <div style={{ textAlign: "center" }}>
+
+      </div>
+  )
+}
+
+const Page = () => {
+  const siteMetadata = useSiteMetadata();
+  
+  const data = useStaticQuery(graphql`
+    {
+      projectJson(slug: { eq: "step-indicator-scroll-snap" }) {
+        ...projectFragment
+      }
+    }
+  `);
+
+  const project = data.projectJson;
+
+  return (
+    <ProjectLayout
+      projectTitle={project.title}
+      projectStatus={project.status}
+      projectVersion={project.version}
+      projectCoverImage={project.cover_image.childImageSharp.gatsbyImageData}
+      projectDescription={project.description}
+      projectDependencies={project.dependencies}
+      projectMode={project.mode}
+    >
+      <Seo 
+        customTitle={`${project.title} | ${siteMetadata.title}`}
+        customDescription={project.description}
+        customURL={`${siteMetadata.url}/project/${slugify(project.title)}`}
+        customOgImage={`${siteMetadata.url}${project.cover_image.publicURL}`}
+      />
+      <Project/>
+    </ProjectLayout>
+  );
+};
+
+export default Page;
