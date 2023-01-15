6b58d669e536b0e19890d1a2d22e491cf08ccd6c
Fong Say Kiat <65012113+fsk1997@users.noreply.github.com>
2023-01-15 22:35:45 +0800

    Optimise image load speed

--- src/pages/project/dialog-height-transition/index.js
+++ src/pages/project/dialog-height-transition/index.js
@@ -34,93 +34,92 @@ const Project = () => {
   const tabContent = [
     {
       id: 1,
-      name:"☃️",
+      name: "☃️",
       children: <><img src="https://newjeans.kr/imgs/window/pc/icon-melody-card.gif" /></>,
     },
     {
       id: 2,
-      name:"🐱",
-      children: <><img src="https://newjeans.kr/imgs/window/new-folder-1/nj_ditto_16.jpg" /></>,
+      name: "🐱",
+      children: <><img loading="lazy" src="https://newjeans.kr/imgs/window/new-folder-1/nj_ditto_16.jpg" /></>,
     },
     {
       id: 3,
-      name:"🍬",
-      children: <><img src="https://newjeans.kr/imgs/window/new-folder-1/nj_ditto_12.jpg" /></>,
+      name: "🍬",
+      children: <><img loading="lazy" src="https://newjeans.kr/imgs/window/new-folder-1/nj_ditto_12.jpg" /></>,
     },
     {
       id: 4,
-      name:"🧡",
-      children: <><img src="https://newjeans.kr/imgs/window/new-folder-1/nj_omg_2.jpg" /></>,
+      name: "🧡",
+      children: <><img loading="lazy" src="https://newjeans.kr/imgs/window/new-folder-1/nj_omg_2.jpg" /></>,
     },
     {
       id: 5,
-      name:"🧸",
-      children: <><iframe style={{width:"100%"}} width="560" height="315" src="https://www.youtube.com/embed/sVTy_wmn5SU" title="YouTube video player" frameborder="0" allowfullscreen/></>,
+      name: "🧸",
+      children: <><iframe style={{ width: "100%" }} width="560" height="315" src="https://www.youtube.com/embed/sVTy_wmn5SU" title="YouTube video player" frameborder="0" allowfullscreen /></>,
     },
   ]
 
   return (
     <div className={styles.page}>
       <button className={styles.button} onClick={() => initiateDialog()}>New Jeans</button>
-      {showDialog &&
-        <div className={styles.dialog}>
-          <div
-            className={styles.dialogBackground}
-            style={null}
-          ></div>
-          <div className={styles.dialogInner}>
-            <div className={styles.dialogNav}>
-              <div className={styles.toggles}>
-                {tabContent.map((item) => {
-                  return (
-                    <button key={item.id} className={styles.button} onClick={() => fetchTab(item.id)}>{item.name}</button>
-                  )
-                })}
-              </div>
-              <button onClick={() => closeDialog()}>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="16"
-                  height="16"
-                  fill="#000000"
-                  viewBox="0 0 256 256"
-                >
-                  <rect width="256" height="256" fill="none"></rect>
-                  <line
-                    x1="200"
-                    y1="56"
-                    x2="56"
-                    y2="200"
-                    fill="none"
-                    stroke="#000000"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="24"
-                  ></line>
-                  <line
-                    x1="200"
-                    y1="200"
-                    x2="56"
-                    y2="56"
-                    fill="none"
-                    stroke="#000000"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="24"
-                  ></line>
-                </svg>
-              </button>
+      <div className={styles.dialog + " " + (showDialog && styles.show)}>
+        <div
+          className={styles.dialogBackground}
+          style={null}
+        ></div>
+        <div className={styles.dialogInner}>
+          <div className={styles.dialogNav}>
+            <div className={styles.toggles}>
+              {tabContent.map((item) => {
+                return (
+                  <button key={item.id} className={styles.button} onClick={() => fetchTab(item.id)}>{item.name}</button>
+                )
+              })}
             </div>
-            {tabContent.map((item) => {
-              return (
-                <div ref={dialogRef} className={(styles.content) + " " + (tab === item.id && styles.active)}>
-                  {item.children}
-                </div>
-              )
-            })}
+            <button onClick={() => closeDialog()}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="#000000"
+                viewBox="0 0 256 256"
+              >
+                <rect width="256" height="256" fill="none"></rect>
+                <line
+                  x1="200"
+                  y1="56"
+                  x2="56"
+                  y2="200"
+                  fill="none"
+                  stroke="#000000"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="24"
+                ></line>
+                <line
+                  x1="200"
+                  y1="200"
+                  x2="56"
+                  y2="56"
+                  fill="none"
+                  stroke="#000000"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="24"
+                ></line>
+              </svg>
+            </button>
           </div>
+          {tabContent.map((item) => {
+            return (
+              <div ref={dialogRef} className={(styles.content) + " " + (tab === item.id && styles.active)}>
+                {item.children}
+              </div>
+            )
+          })}
         </div>
-      }
+      </div>
+
     </div>
   )
 }
