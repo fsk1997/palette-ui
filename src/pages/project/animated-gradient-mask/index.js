6717b7cd023addf4264c39a28cfc5f18354b5e18
Fong Say Kiat <65012113+fsk1997@users.noreply.github.com>
2023-03-12 10:59:23 +0800

    Simplify project

--- src/pages/project/animated-gradient-mask/index.js
+++ src/pages/project/animated-gradient-mask/index.js
@@ -14,14 +14,10 @@ const Project = () => {
 
   return (
     <div className={styles.page}>
-      {animate && 
-        <div className={styles.wrapper + " " + (animate === true ? styles.animateIn : styles.blank)} style={{aspectRatio: "9/16"}}>
-          {/* <iframe width="560" height="500" src="https://youtube.com/embed/tsQ3lCWV5PY?autoplay=1&mute=1&playlist=tsQ3lCWV5PY&loop=1&controls=0&disablekb=0&" title="YouTube video player" /> */}
-          <img src="https://kpopping.com/documents/d5/5/1844-image-20221029143647-2-gif-gif.jpeg?v=25590" alt="newjeans x mcdonalds kr"/>    
-        </div>
-      }
-
-      <button className={styles.button} onClick={()=>setAnimate(!animate)}>{animate ? "Toggle Off" : "Toggle On" }</button>
+      <button className={styles.button} onClick={()=>setAnimate(!animate)}>{animate ? "Tap Again" : "Tap Me" }</button>
+      <div className={styles.wrapper + " " + (animate && styles.show)} style={{aspectRatio: "9/21"}}>
+        <img src="https://kpopping.com/documents/d5/5/1844-image-20221029143647-2-gif-gif.jpeg?v=25590" alt="Rotating Newjeans fan stick"/>    
+      </div>
     </div>
   );
 };
@@ -31,7 +27,7 @@ const Page = () => {
 
   const data = useStaticQuery(graphql`
     {
-      projectJson(slug: { eq: "animated-gradient-mask" }) {
+      projectJson(slug: { eq: "gradient-mask-transition" }) {
         ...projectFragment
       }
     }
